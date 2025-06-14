647d18b290afb39ca6cc6209ce6e1b895078dbdc
vaibhavM <vaibhav@kali.kali>
2025-06-15 01:13:49 +0530

    cloudianry image upload, useOptimistic hook to show liked before happning on server

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,44 +1,6 @@
-import { FormSubmit } from "@/components/form-submit";
-import { storePost } from "@/lib/posts";
-//import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
+import { createPost } from "@/actions/posts";
+import { PostForm } from "@/components/post-form";
 
-const NewPostPage = () => {
+const NewPostPage = () => <PostForm action={createPost}/>;
 
-  const createPost = async (formData) => {
-    'use server';
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
-    await storePost({ imageUrl: '', title, content, userId: 1 });
-    //revalidatePath('/feed');
-    redirect('/feed');
-  }
-
-  return <>
-  <h1>Create a new post</h1>
-  <form action={createPost}>
-    <p className="form-control">
-      <label htmlFor="title">Title</label>
-      <input type="text" id="title" name="title" />
-    </p>
-    <p className="form-control">
-      <label htmlFor="image">Image URL</label>
-      <input
-        type="file"
-        accept="image/png, image/jpeg"
-        id="image"
-        name="image"
-      />
-    </p>
-    <p className="form-control">
-      <label htmlFor="content">Content</label>
-      <textarea id="content" name="content" rows="5" />
-    </p>
-    <p className="form-actions">
-      <FormSubmit/>
-    </p>
-  </form>
-</>;
-}
 export default NewPostPage;
--- components/posts.js
+++ components/posts.js
@@ -1,9 +1,12 @@
+'use client';
 import { formatDate } from '@/lib/format';
 import { LikeButton } from './like-icon';
+import { togglePostLikeStatus } from '@/actions/posts';
+import { useOptimistic } from 'react';
 
-const Post = ({ post }) => <article className="post">
+const Post = ({ post, action }) => <article className="post">
   <div className="post-image">
-    <img src={null} alt={post.title} />
+    <img src={post.image} alt={post.title} />
   </div>
   <div className="post-content">
     <header>
@@ -16,8 +19,10 @@ const Post = ({ post }) => <article className="post">
           </time>
         </p>
       </div>
-      <div>
-        <LikeButton />
+      <div className={post.isLiked ? 'liked' : ''}>
+        <form action={action.bind(null, post.id)}>
+          <LikeButton />
+        </form>
       </div>
     </header>
     <p>{post.content}</p>
@@ -26,12 +31,31 @@ const Post = ({ post }) => <article className="post">
 
 
 export const Posts = ({ posts }) => {
-  if (!posts || posts.length === 0) return <p>There are no posts yet. Maybe start sharing some?</p>;
+  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(posts, (prevPosts, updatedPostId) => {
+    const updatedPostIndex = prevPosts.findIndex(post => post.id === updatedPostId);
+
+    if(updatedPostIndex === -1) return prevPosts;
+
+    const updatedPost = { ...prevPosts[updatedPostIndex] };
+    updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
+    updatedPost.isLiked = !updatedPost.isLiked;
+    const newPosts = [...prevPosts];
+    newPosts[updatedPostIndex] = updatedPost;
+
+    return newPosts;
+  }); //To show instant like before updating it in database.
+
+  if (!optimisticPosts || optimisticPosts.length === 0) return <p>There are no posts yet. Maybe start sharing some?</p>;
+
+  const updatePost = async (postId) => {
+    updateOptimisticPosts(postId);
+    await togglePostLikeStatus(postId);
+  } 
 
   return <ul className="posts">
-    {posts.map((post) => (
+    {optimisticPosts.map((post) => (
       <li key={post.id}>
-        <Post post={post} />
+        <Post post={post} action={updatePost}/>
       </li>
     ))}
   </ul>;
